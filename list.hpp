422083f0b7470f6d3f815c4e8efe726799627ac5
Drogo Skittrick <dskittri@ox-g5.msk.21-school.ru>
2021-04-14 20:09:25 +0300

    no last

--- list.hpp
+++ list.hpp
@@ -15,6 +15,8 @@ namespace ft
 		typedef	T							value_type;
 		typedef	ptrdiff_t					difference_type;
 		typedef	size_t						size_type;
+        
+
 
 		private:
 			class node
@@ -23,15 +25,23 @@ namespace ft
 					T				data;
 					node			*next;
 					node			*prev;
-					size_t			size;
 					node() {}
 					node(T d) : data(d){}
 					~node() {}
 			};
-        public:
-			node 				*tail;
+			size_type			_size;
+
+			static	bool	my_comp(value_type &first, value_type &second)
+			{
+				if (first < second)
+					return (true);
+				else
+					return (false);
+			};
 
         public:
+
+			node 				*tail;
 
 			//CONSTRUCTOR
 
@@ -42,7 +52,7 @@ namespace ft
 				tail->prev = tail;
 				tail->next = tail;
 				tail->data = 0;
-				tail->size = 0;
+				_size = 0;
 				//сделать итератор на tail
 				//tail_it = iterator(tail);
 			};
@@ -54,7 +64,7 @@ namespace ft
 				tail->prev = tail;
 				tail->next = tail;
 				tail->data = 0;
-				tail->size = 0;
+				_size = 0;
 				for (int i = 0; i != n; i++)
 					push_back(val);
 				//как мы засовывваем именно копию?
@@ -69,7 +79,7 @@ namespace ft
 				tail->prev = tail;
 				tail->next = tail;
 				tail->data = 0;
-				tail->size = 0;
+				_size = 0;
 				std::cout << "\nIterator constructor is called!\n";
 				//std::cout << first;
 				// for (;first != last; first++)
@@ -82,7 +92,7 @@ namespace ft
 				tail->prev = tail;
 				tail->next = tail;
 				tail->data = 0;
-				tail->size = 0;
+				_size = 0;
 				//удалить все, что было
 				//обнулить tail
 				node	*tmp = x.tail->next;
@@ -104,8 +114,7 @@ namespace ft
                 tmp = new node;
                 tmp->data = val;
 
-                tmp->size = 0;
-                if (tail->size == 0)
+                if (_size == 0)
                 {
                     tmp->prev = tail;
                     tail->next = tmp;
@@ -117,7 +126,7 @@ namespace ft
                 }
                 tmp->next = tail;
                 tail->prev = tmp;
-                tail->size = tail->size + 1;
+                _size = _size + 1;
             };			
 
 			void push_front (const value_type& val)
@@ -126,7 +135,7 @@ namespace ft
 
                 tmp = new node;
                 tmp->data = val;
-				if (tail->size == 0)
+				if (_size == 0)
 				{
 					tmp->next = tail;
 					tail->prev = tmp;
@@ -138,53 +147,246 @@ namespace ft
 				}
 				tmp->prev = tail;
 				tail->next = tmp;
-				tail->size = tail->size + 1;			
+				_size = _size + 1;			
 			};
 
 			void pop_front()
 			{
-				if (tail->size != 0)
+				if (_size != 0)
 				{
 					node *tmp = tail->next;
 					tail->next = tmp->next;
 					tmp->next->prev = tail;
-					tail->size = tail->size - 1;
+					_size = _size - 1;
 					delete tmp;
 				}
 			};
 			void pop_back()
 			{
-				if (tail->size != 0)
+				if (_size != 0)
 				{
 					node *tmp = tail->prev;
 					tmp->prev->next = tail;
 					tail->prev = tmp->prev;
-					tail->size = tail->size - 1;
+					_size = _size - 1;
 					delete tmp;
 				}				
 			};
 
 			void swap (list& x)
 			{
-				node 	*first;
-				node	*second;
-				size_t	size;
+				node 	*prev_tail_first;
+				node	*next_tail_first;
+				size_t	_size;
 
-				// first = this->tail->next;
-				// second = this->tail->prev;
-				// size = this->tail->size;
+
+				next_tail_first = this->tail->next;
+				prev_tail_first = this->tail->prev;
+				_size = this->_size;
+
+				//Work with this
+
 				this->tail->next = x.tail->next;
 				this->tail->prev = x.tail->prev;
-				this->tail->size = x.tail->size;
-				// x.tail->next = first;
-				// x.tail->prev = second;
-				// x.tail->size = size;
+				this->_size = x._size;
+				x.tail->next->prev = this->tail;
+				x.tail->prev->next = this->tail;
+
+				//Work with x
+
+				x.tail->next = next_tail_first;
+				x.tail->prev = prev_tail_first;
+				x._size = _size;
+				next_tail_first->prev = x.tail;
+				prev_tail_first->next = x.tail;
 			}
+			void resize (size_type n, value_type val = value_type())
+			{
+				if (n < this->_size)
+				{
+					while (this->_size != n)
+						pop_back();
+				}
+				else if (n > this->_size)
+				{
+					while (this->_size != n)
+						push_back(val);
+				}
+			};
+
+			void clear()
+			{
+				while (this->_size != 0)
+					pop_back();
+			};
+
+			bool empty() const
+			{
+				if (_size == 0)
+					return (true);
+				else
+					return (false);
+			};
+
+			size_type max_size() const
+			{
+				return (std::numeric_limits<size_type>::max() / sizeof(node));
+			};
+			size_type size() const
+			{
+				return (_size);
+			};
+
+			void remove (const value_type& val)
+			{
+				node *tmp = tail->next;
+				while (tmp != tail)
+				{
+					if (tmp->data == val)
+					{
+						node *next = tmp->next;
+						tmp->prev->next = tmp->next;
+						tmp->next->prev = tmp->prev;
+						delete tmp;
+						tmp = next;
+						_size = _size - 1;
+					}
+					else
+						tmp = tmp->next;
+				}
+			};
+			template <class Predicate>
+			void remove_if (Predicate pred)//Нужно проверить
+			{
+				node *tmp = tail->next;
+				while (tmp != tail)
+				{
+					if (pred(tmp->data) == true)
+					{
+						node *next = tmp->next;
+						tmp->prev->next = tmp->next;
+						tmp->next->prev = tmp->prev;
+						delete tmp;
+						tmp = next;
+						_size = _size - 1;
+					}
+					else
+						tmp = tmp->next;
+				}
+			};
+
+			void reverse()
+			{
+				node *tmp = tail->next->next;
+				node *tmp_tail = tail; 	
+				while (tmp != tail)
+				{
+					node *next = tmp->next;
+					std::swap(tmp->next, tmp->prev);
+					tmp = next;
+				}
+				tail->prev->next = tmp_tail;
+			};
+
+
+
+
+			// void unique()
+			// {
+
+			// };
+			// template <class BinaryPredicate>
+			// void unique (BinaryPredicate binary_pred)
+			// {
+
+			// };
+
+			// template <class Compare>
+			// void merge (list& x, Compare comp)
+			// {
+			// 	bool	if_changed = true;
+			// 	node	*tmp;
+
+			// 	if (&x != this)
+			// 	{
+			// 		std::cout << "start" << std::endl;
+			// 		tmp = tail->next;
+			// 		while (if_changed)
+			// 		{
+			// 			// if (tmp->next == tail)
+			// 			// {
+			// 			// 	_size = _size + x._size;
+			// 			// 	tmp->next
+			// 			// 	if_changed = false;
+			// 			// }
+			// 			// else 
+			// 			if (comp(tmp->next->data, x.tail->next->data) == false || tmp->next == tail)
+			// 			{
+			// 				node *second = tmp->next;
+			// 				std::cout << tmp->data << std::endl;
+			// 				std::cout << second->data << std::endl;
+			// 				_size = _size + x._size;
+			// 				tmp->next = x.tail->next;
+			// 				x.tail->next->prev = tmp;
+			// 				second->prev = x.tail->prev;
+			// 				x.tail->prev->next = second;
+			// 				second->prev = x.tail->prev;
+			// 				if_changed = false;
+			// 			}
+			// 			else
+			// 				tmp = tmp->next;
+			// 		}
+			// 		x.tail->prev = x.tail;
+			// 		x.tail->next = x.tail;
+			// 		x._size = 0;
+			// 	}
+			// 	//удалить tail
+			// };
+
+			// void merge (list& x)
+			// {
+			// 	merge(x, my_comp);
+			// };
+
+			// template <class Compare>
+			// void sort (Compare comp)
+			// {
+			// 	node	*tmp = tail->next;
+			// 	bool	swapped = true;
+
+			// 	while (swapped)
+			// 	{
+			// 		tmp = tail->next;
+			// 		swapped = false;
+			// 		while (tmp != tail)
+			// 		{
+			// 			if (tmp->next != tail && comp(tmp->data, tmp->next->data) == false)
+			// 			{
+			// 				node *tmp2;
+			// 				tmp2 = tmp->next;
+			// 				tmp2->next->prev = tmp;
+			// 				tmp->prev->next = tmp2;
+			// 				tmp->next = tmp2->next;
+			// 				tmp2->next = tmp;
+			// 				tmp2->prev = tmp->prev;
+			// 				tmp->prev = tmp2;
+			// 				swapped = true;
+			// 			}
+			// 			else
+			// 				tmp = tmp->next;
+			// 		}
+			// 	}
+			// };
+
+			// void sort()
+			// {
+			// 	this->sort(my_comp);
+			// };
 
             void	print_all()
             {
                 node *tmp = tail;
-                std::cout << "\nSize of List is " << tail->size << "\n";
+                std::cout << "\nSize of List is " << _size << "\n";
                 tmp = tmp->next;
                 int i = 1;
                 while (tmp != tail)
--- main.cpp
+++ main.cpp
@@ -2,6 +2,10 @@
 #include <list>
 #include "list.hpp"
 
+struct is_odd {
+  bool operator() (const int& value) { return (value%2)==1; }
+};
+
 int main(void)
 {
 	ft::list<int>	first;
@@ -44,10 +48,90 @@ int main(void)
 	first.print_all();
 	std::cout << "second\n";
 	second.print_all();
-	first.swap(forth);
+	first.swap(second);
 	std::cout << "first\n";
 	first.print_all();
 	std::cout << "second\n";
 	second.print_all();
+	std::cout << "\n\t\tCheck Resize\n";
+	std::cout << "first.resize(5,42);\n";
+	first.resize(5,42);
+	first.print_all();
+	std::cout << "first.resize(3,42);\n";
+	first.resize(3,42);
+	first.print_all();
+	std::cout << "first.resize(5,42);\n";
+	first.resize(5,42);
+	first.print_all();
+	std::cout << "\n\t\tRemove Check\n";
+	std::cout << "first.remove(42);\n";
+	first.remove(42);
+	first.print_all();
+
+	std::cout << "\n\t\tRemove If Check\n";
+	first.push_back(2);
+	first.push_back(4);
+	first.push_back(6);
+	std::cout << "first.remove_if (is_odd());\n";
+	first.print_all();
+	first.remove_if (is_odd());
+	first.print_all();
+
+	std::cout << "\n\t\tCheck Clear\n";
+	std::cout << "first.clear();\n";
+	first.clear();
+	first.print_all();
+
+	std::cout << "\n\t\tCheck Max size\n";
+	std::cout << "\nMax size: " << first.max_size() << "\n";
+	std::list<int> original;
+	std::cout << "\nMax size: " << original.max_size() << "\n";
+
+	std::cout << "\n\t\tCheck Sort\n";
+	first.push_back(3);
+	first.push_back(2);	
+	first.push_back(1);
+	first.push_back(0);
+	first.print_all();
+/*	first.sort();
+	first.print_all();
+	first.push_back(200);
+	first.push_back(201);
+	std::cout << "\n\t\tCheck Merge\n";
+	first.print_all();
+	second.print_all();
+	first.merge(second);
+	first.print_all();
+	ft::list<int>	six;
+	six.push_back(300);
+	six.push_back(301);
+	six.push_back(302);
+	six.print_all();
+	first.merge(six);
+	first.print_all();
+	six.print_all();
+	six.push_back(1);
+	six.push_back(10);
+	six.push_back(100);
+	first.merge(six);
+	first.print_all();
+
+	std::list<int> original1;
+	std::list<int> original2;
+	original1.push_back(100);
+	original1.push_back(2);
+	original1.push_back(40);
+	original2.push_back(1);
+	original2.push_back(10);
+	original2.push_back(20);
+	original1.merge(original2);
+	for(std::list<int>::iterator it = original1.begin(); it != original1.end(); it++)
+		std::cout << *it << "\t";*/
+	// int i;
+	// while (1)
+	// 	i++;
+	std::cout << "\n\t\tCheck Reverse\n";
+	first.reverse();
+	first.print_all();
 	return (0);
 }
